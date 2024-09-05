c140ad01a5de4b98418a91d7c155010da7abdfc0
fai7h01 <l.varsimashvil10@gmail.com>
2024-09-05 21:48:16 +0400

    Initial commit

---
+++ src/test/java/com/cydeo/usermicroservice/UserMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.usermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
