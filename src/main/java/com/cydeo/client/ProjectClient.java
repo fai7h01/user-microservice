3d1bb80ca5520ac8ac1ee64fbd51c3cda4281cf3
fai7h01 <l.varsimashvil10@gmail.com>
2024-09-12 17:07:29 +0400

    client config

--- src/main/java/com/cydeo/client/ProjectClient.java
+++ src/main/java/com/cydeo/client/ProjectClient.java
@@ -5,9 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(value = "project-microservice", url = "localhost:8082")
+@FeignClient(value = "project-service")
 public interface ProjectClient {
 
     @GetMapping("/api/v1/project/count/manager/{assignedManager}")
--- src/main/java/com/cydeo/client/TaskClient.java
+++ src/main/java/com/cydeo/client/TaskClient.java
@@ -5,9 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(value = "task-microservice", url = "localhost:8083")
+@FeignClient(value = "task-service")
 public interface TaskClient {
 
     @GetMapping("/api/v1/task/count/employee/{assignedEmployee}")
